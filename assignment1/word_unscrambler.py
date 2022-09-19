3af4825a3348884a30bc5316db24d5a15ba6cbd1
AAI8219 <aai8@pitt.edu>
2022-09-19 12:22:49 -0400

    Update word_unscrambler.py
    word unscrambler

--- assignment1/word_unscrambler.py
+++ assignment1/word_unscrambler.py
@@ -1,1 +1,78 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# In[8]:
+
+
+user_input = input("Please enter a word: ") #asks user for input
+f = open('words.txt') #opens file 
+data = f.readlines() #read in file line by line
+sorted_user_input = sorted(user_input) #sorts alphabetically 
+results = [] 
+
+
+# In[9]:
+
+
+for word in data:
+    sorted_dict = sorted(word.strip().lower()) 
+    if all(elem in sorted_user_input for elem in sorted_dict):  
+        results.append(word.strip()) #if word exists in dict append to results 
+
+
+# In[10]:
+
+
+sorted_word_list = sorted(results, key = len) #sorts appended list into new sorted_word_list
+
+
+# In[11]:
+
+
+print("All 6 lettered words:") #print message all 6 lettered words - words created from unscrambled user input word
+for word2 in sorted_word_list: 
+    if (len(word2) == 6): #include only words that have 6 letters
+        print(word2)
+
+
+# In[12]:
+
+
+print("All 5 lettered words:")
+for word3 in sorted_word_list:
+    if (len(word3) == 5):
+        print(word3)
+
+
+# In[13]:
+
+
+print("All 4 lettered words:")
+for word4 in sorted_word_list:
+    if (len(word4) == 4):
+        print(word4)
+
+
+# In[14]:
+
+
+print("All 3 lettered words:")
+for word5 in sorted_word_list:
+    if (len(word5) == 3):
+        print(word5)
+
+
+# In[ ]:
+
+
+
+
+
+# In[ ]:
+
+
+
+
+
+# In[ ]:
 
